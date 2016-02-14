13b5a1b77062de7a82106a3db4951f883292b1dc
Hector Huertas <hectorhuertas@gmail.com>
2016-02-13 18:23:25 -0700

    Initial commit - (After finishing 'Adding Pages')

---
+++ web/controllers/hello_controller.ex
@@ -0,0 +1,12 @@
+defmodule HelloPhoenix.HelloController do
+  use HelloPhoenix.Web, :controller
+
+  def index(conn, _params) do
+    # render conn, "index.html"
+    render conn, :index
+  end
+
+  def show(conn, %{"messenger" => messengerX}) do
+    render conn, :show, messenger: messengerX
+  end
+end
